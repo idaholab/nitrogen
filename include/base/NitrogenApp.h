41bd5ef8b077f23fd7a21f5f7855279098548299
David Andrs <david.andrs@inl.gov>
2020-04-06 14:34:17 -0600

    Updating validParams to use static method
    Refs idaholab/moose#14135

--- include/base/NitrogenApp.h
+++ include/base/NitrogenApp.h
@@ -3,18 +3,14 @@
 #include "MooseApp.h"
 
 class Factory;
-class NitrogenApp;
-
-template <>
-InputParameters validParams<NitrogenApp>();
 
 class NitrogenApp : public MooseApp
 {
 public:
   NitrogenApp(InputParameters parameters);
 
+public:
+  static InputParameters validParams();
   static void registerApps();
   static void registerAll(Factory & f, ActionFactory & af, Syntax & s);
-
-protected:
 };
